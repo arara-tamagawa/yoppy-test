c0bf42488f764900d21d38105c6627223e5b1561
tamagawa <tamagawa@arara.com>
2021-08-13 18:58:39 +0900

    up

--- yoppy_lambda/lambda_function.py
+++ yoppy_lambda/lambda_function.py
@@ -1,18 +1,6 @@
-import json
-#import boto3
 from datetime import datetime, timedelta, timezone
-import pprint
-#from boto3.dynamodb.conditions import Key, Attr
 import pandas as pd
 import numpy as np
-
-#dynamodb指定
-# DYNAMO_TABLE_NAME = 'yoppy-test-db2'    #要確認
-# dynamodb = boto3.resource('dynamodb')
-# table    = dynamodb.Table(DYNAMO_TABLE_NAME)
-# #s3指定
-# s3_resource = boto3.resource('s3')
-# OUTPUT_BUCKET = 'log-yoppy-csv'         #要確認
 
 
 def handle_date():
@@ -34,16 +22,7 @@ def handle_date():
     global LAST_MONTH_OUTPUT_KEY 
     LAST_MONTH_OUTPUT_KEY =  year_only_str + '/' + month_only_str + '/食堂利用情報_' + year_only_str + '_' + month_only_str + '.csv'   #2021/07/export.csv
 
-def get_table(year_lastmonth_information):
-    options = {
-        'FilterExpression': Attr('used_at_date').contains(year_lastmonth_information),
-        'ProjectionExpression' : 'mail_address,used_at_date,used_in,reservation_num'
-    }
-    response = table.scan(**options)
-    #確認用
-    #pprint.pprint(response)
-    return response
-    
+
 def export_csv(f):
     #dynamoDBからアイテムを取得
     df = pd.DataFrame(f)
@@ -56,11 +35,3 @@ def export_csv(f):
     #csvファイルにエクスポート
     data = df2.to_csv(TEMP_FILENAME, index=False, header=True)
     return data
-#if __name__ == '__main__':
-def lambda_handler(event, context):
-    handle_date()
-    csv_file=export_csv(get_table(lastmonth_str)['Items'])
-    #s3にアップロード
-    #s3_resource.Bucket(OUTPUT_BUCKET).upload_file(TEMP_FILENAME, LATEST_OUTPUT_KEY)
-    #s3_resource.Bucket(OUTPUT_BUCKET).upload_file(TEMP_FILENAME, LAST_MONTH_OUTPUT_KEY)    
-    return 0
