c395d83869d4414e48b9ae755e7f4850c037c205
tamagawa <tamagawa@arara.com>
2021-08-16 16:39:51 +0900

    sesss

--- yoppy_lambda/test_lambda_function.py
+++ yoppy_lambda/test_lambda_function.py
@@ -20,11 +20,11 @@ def test_handle_date():
 
 
 def test_export_csv():
-    response={'Count': 2,'Items': [{'mail_address': 'okuhara@arara.com','reservation_num': 3,'used_at_date': '2021-07-31','used_in': 'Happy食堂'},{'mail_address': 'tamagawa@arara.com','reservation_num': 1,'used_at_date': '2021-07-31','used_in': 'Happy食堂'}],'ResponseMetadata': {'HTTPHeaders': {'connection': 'keep-alive','content-length': '309','content-type': 'application/x-amz-json-1.0','date': 'Fri, 13 Aug 2021 06:51:55 GMT','server': 'Server','x-amz-crc32': '111695128','x-amzn-requestid': '9LAFQD1E1TBD8NDAOJF9M3VL5BVV4KQNSO5AEMVJF66Q9ASUAAJG'},'HTTPStatusCode': 200,'RequestId': '9LAFQD1E1TBD8NDAOJF9M3VL5BVV4KQNSO5AEMVJF66Q9ASUAAJG','RetryAttempts': 0},'ScannedCount': 8}
+    response={'Count': 2,'Items': [{'mail_address': 'okuhara@arara.com','reservation_num': 3,'used_at_date': '2021-08-31','used_in': 'Happy食堂'},{'mail_address': 'tamagawa@arara.com','reservation_num': 1,'used_at_date': '2021-07-31','used_in': 'Happy食堂'}],'ResponseMetadata': {'HTTPHeaders': {'connection': 'keep-alive','content-length': '309','content-type': 'application/x-amz-json-1.0','date': 'Fri, 13 Aug 2021 06:51:55 GMT','server': 'Server','x-amz-crc32': '111695128','x-amzn-requestid': '9LAFQD1E1TBD8NDAOJF9M3VL5BVV4KQNSO5AEMVJF66Q9ASUAAJG'},'HTTPStatusCode': 200,'RequestId': '9LAFQD1E1TBD8NDAOJF9M3VL5BVV4KQNSO5AEMVJF66Q9ASUAAJG','RetryAttempts': 0},'ScannedCount': 8}
     export_csv(response['Items'],a1)
 
     test_frame=pd.read_csv(a1)
-    assert not test_frame.isnull().values.sum()
+    assert not test_frame.isnull().values.sum() #csvにnullがないことを確認
     date_list=test_frame.利用日.tolist()
     for x in date_list:
-        assert x.startswith(a0)
+        assert x.startswith(a0)  #csv内の利用日項目が指定した年月のものだけであること
