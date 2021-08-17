0e06ed151bc673772db90b89fef3a2bd590ec839
tamagawa <tamagawa@arara.com>
2021-08-17 16:43:18 +0900

    test

--- yoppy_lambda/lambda_function.py
+++ yoppy_lambda/lambda_function.py
@@ -29,12 +29,12 @@ def handle_date():
     year_only_str=lastmonth.strftime("%Y")
     month_only_str=lastmonth.strftime("%m")
     
-    TEMP_FILENAME = '/tmp/食堂利用情報_' + year_only_str + '_' + month_only_str + '.csv' #/tmp/のパスが必要
-    LATEST_OUTPUT_KEY = 'latest/食堂利用情報_' + year_only_str + '_' + month_only_str + '.csv' #上書きされる
-    LAST_MONTH_OUTPUT_KEY =  year_only_str + '/' + month_only_str + '/食堂利用情報_' + year_only_str + '_' + month_only_str + '.csv'   #2021/07/export.csv
-    print(TEMP_FILENAME)
-    print(LATEST_OUTPUT_KEY)
-    print(LAST_MONTH_OUTPUT_KEY)
+    TEMP_FILENAME = '/tmp/yoppylog' #/tmp/のパスが必要
+    LATEST_OUTPUT_KEY = 'monthly/latest/yoppylog' #上書きされる
+    LAST_MONTH_OUTPUT_KEY =  year_only_str + '/' + month_only_str + '/yoppylog'   #2021/07/yoppylog
+    #print(TEMP_FILENAME)
+    #print(LATEST_OUTPUT_KEY)
+    #print(LAST_MONTH_OUTPUT_KEY)
     return [lastmonth_str,TEMP_FILENAME,LATEST_OUTPUT_KEY,LAST_MONTH_OUTPUT_KEY]
 
 def export_csv(frame,temp_filename):
@@ -45,9 +45,9 @@ def export_csv(frame,temp_filename):
     df2 = df2.assign(ゲスト有無=lambda x: np.where((x['reservation_num']>1),'1','0'))
     df2 = df2.rename(columns={'mail_address':'利用者','used_at_date':'利用日','used_in':'利用場所','reservation_num':'利用人数'})
     #確認用
-    print(df2.to_string())
+    #print(df2.to_string())
     #csvファイルにエクスポート
-    data = df2.to_csv(temp_filename, index=False, header=True)
+    df2.to_csv(temp_filename, index=False, header=True)
 
 def get_table(year_lastmonth_information):
     options = {
@@ -56,19 +56,16 @@ def get_table(year_lastmonth_information):
     }
     response = table.scan(**options)
     #確認用
-    print('-----------response------------------')
-    print(type(response))
-    pprint.pprint(response)
-    print('-------------------------------------')
+    #print('-----------response------------------')
+    #print(type(response))
+    #pprint.pprint(response)
+    #print('-------------------------------------')
     return response
 
 def lambda_handler(event, context):
     environment()
     url_info=handle_date()
-    #csv_file=
     export_csv(get_table(url_info[0])['Items'],url_info[1])
     #s3にアップロード
     s3_resource.Bucket(OUTPUT_BUCKET).upload_file(url_info[1], url_info[2])
     s3_resource.Bucket(OUTPUT_BUCKET).upload_file(url_info[1], url_info[3])
-    
-    return 0
--- yoppy_lambda/test_lambda_function.py
+++ yoppy_lambda/test_lambda_function.py
@@ -10,20 +10,21 @@ def test_handle_date():
     global a0    
     a0=lastmonth.strftime("%Y-%m")
     global a1
-    a1='/tmp/食堂利用情報_'+lastmonth.strftime("%Y")+'_'+lastmonth.strftime("%m")+'.csv'
-    a2='latest/食堂利用情報_'+lastmonth.strftime("%Y")+'_'+lastmonth.strftime("%m")+'.csv'
-    a3=lastmonth.strftime("%Y")+'/'+lastmonth.strftime("%m")+'/食堂利用情報_'+lastmonth.strftime("%Y")+'_'+lastmonth.strftime("%m")+'.csv'
+    a1='/tmp/yoppylog'
+    a2='monthly/latest/yoppylog'
+    a3=lastmonth.strftime("%Y")+'/'+lastmonth.strftime("%m")+'/yoppylog'
     assert x[0]==a0 #'2021-07'
-    assert x[1]==a1#'/tmp/食堂利用情報_2021_07.csv'
-    assert x[2]==a2#'latest/食堂利用情報_2021_07.csv'
-    assert x[3]==a3#'2021/07/食堂利用情報_2021_07.csv'
+    assert x[1]==a1#'/tmp/yoppylog'
+    assert x[2]==a2#'monthly/latest/yoppylog'
+    assert x[3]==a3#'2021/07/yoppylog'
 
 
 def test_export_csv():
+    #仮データ
     response={'Count': 2,'Items': [{'mail_address': 'okuhara@arara.com','reservation_num': 3,'used_at_date': '2021-07-31','used_in': 'Happy食堂'},{'mail_address': 'tamagawa@arara.com','reservation_num': 1,'used_at_date': '2021-07-31','used_in': 'Happy食堂'}],'ResponseMetadata': {'HTTPHeaders': {'connection': 'keep-alive','content-length': '309','content-type': 'application/x-amz-json-1.0','date': 'Fri, 13 Aug 2021 06:51:55 GMT','server': 'Server','x-amz-crc32': '111695128','x-amzn-requestid': '9LAFQD1E1TBD8NDAOJF9M3VL5BVV4KQNSO5AEMVJF66Q9ASUAAJG'},'HTTPStatusCode': 200,'RequestId': '9LAFQD1E1TBD8NDAOJF9M3VL5BVV4KQNSO5AEMVJF66Q9ASUAAJG','RetryAttempts': 0},'ScannedCount': 8}
     export_csv(response['Items'],a1)
 
-    test_frame=pd.read_csv(a1)
+    test_frame=pd.read_csv(a1)  #csvファイルかどうかはここでわかる
     assert not test_frame.isnull().values.sum() #csvにnullがないことを確認
     date_list=test_frame.利用日.tolist()
     for x in date_list:
